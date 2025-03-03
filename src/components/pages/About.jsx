036bb7cd7ab40b38533e827918e41bc8dfd69d38
JossieHaven <jossiehaven@gmail.com>
2025-03-03 13:20:22 -0600

    Committing About and Contact pages

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,14 @@
+import AboutCard from "../aboutCard";
+export default function About() {
+  return (
+    <div>
+      <h1>A Bit About Me</h1>
+      <AboutCard />
+      <p>
+        
+      </p>
+    </div>
+  );
+}
+
+
---
+++ src/components/pages/Contact.jsx
@@ -0,0 +1,14 @@
+import ContactForm from "../contactForm";
+
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Me</h1>
+      <ContactForm />
+    </div>
+  );
+}
+
+  
+
